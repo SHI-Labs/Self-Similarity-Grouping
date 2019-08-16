c7929900836964ae2ea20e307017a13d577c3e9c
yangfu <fuyang8004@gmail.com>
2019-08-16 10:59:39 -0700

    fix bugs

--- source_train.py
+++ source_train.py
@@ -22,10 +22,10 @@ from reid.utils.data.preprocessor import Preprocessor
 from reid.utils.data.sampler import RandomIdentitySampler
 from reid.utils.logging import Logger
 from reid.utils.serialization import load_checkpoint, save_checkpoint
+
+
 from reid.evaluation_metrics import accuracy
 from reid.utils.meters import AverageMeter
-from reid.eug import *
-
 class Trainer(object):
     def __init__(self, model, criterions, print_freq=1):
         super(Trainer, self).__init__()
@@ -47,7 +47,7 @@ class Trainer(object):
 
             inputs, targets = self._parse_data(inputs)
             loss, prec1 = self._forward(inputs, targets, epoch)
-            losses.update(loss.item(), targets.size(0))
+            losses.update(loss.data[0], targets.size(0))
             precisions.update(prec1, targets.size(0))
 
             optimizer.zero_grad()
@@ -105,57 +105,59 @@ class ResNet(models.ResNet):
         x2 = self.classifier_x2(x2)
         return x1, x2
 
-def get_data(name, data_dir, height, width, batch_size,
-             workers, num_select=1):
+
+def get_data(name, split_id, data_dir, height, width, batch_size, num_instances,
+             workers, combine_trainval):
     root = osp.join(data_dir, name)
 
-    dataset = datasets.create(name, root, num_val=0.1)
+    dataset = datasets.create(name, root, num_val=0.1, split_id=split_id)
 
     normalizer = T.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
 
-    train_set = dataset.trainval # use all training image to train
-    num_classes = dataset.num_trainval_ids
-    
-    if num_select == 1:
-        load_path = './oneshot_split/oneshot_'
-    elif num_select == 3:
-        load_path = './oneshot_split/threeshot_'
-    elif num_select == 5:
-        load_path = './oneshot_split/fiveshot_'
-    elif num_select == 8:
-        load_path = './oneshot_split/eightshot_'
-    else:
-        raise RuntimeWarning('Please choose from 1,3,5,8')
-    
-    load_path = load_path + name + '.pkl'
-    
-    l_data, u_data = get_one_shot_in_cam1(dataset, load_path, num_select)
+    train_set = dataset.trainval if combine_trainval else dataset.train
+    num_classes = (dataset.num_trainval_ids if combine_trainval
+                   else dataset.num_train_ids)
 
-    transformer = T.Compose([
+    train_transformer = T.Compose([
         Resize((height,width)),
+        T.RandomSizedRectCrop(height, width),
+        T.RandomHorizontalFlip(),
         T.ToTensor(),
         normalizer,
-    ]) 
+        T.RandomErasing(probability=0.5,sh=0.2,r1=0.3)
+    ])
 
-    extfeat_loader = DataLoader(
+    test_transformer = T.Compose([
+        T.RectScale(height, width),
+        T.ToTensor(),
+        normalizer,
+    ])
+
+
+    train_loader = DataLoader(
         Preprocessor(train_set, root=dataset.images_dir,
-                     transform=transformer),
+                     transform=train_transformer),
+        batch_size=batch_size, num_workers=workers,
+        sampler=RandomIdentitySampler(train_set, num_instances),
+        pin_memory=True, drop_last=True)
+
+    val_loader = DataLoader(
+        Preprocessor(dataset.val, root=dataset.images_dir,
+                     transform=test_transformer),
         batch_size=batch_size, num_workers=workers,
         shuffle=False, pin_memory=True)
 
     test_loader = DataLoader(
         Preprocessor(list(set(dataset.query) | set(dataset.gallery)),
-                     root=dataset.images_dir, transform=transformer),
+                     root=dataset.images_dir, transform=test_transformer),
         batch_size=batch_size, num_workers=workers,
         shuffle=False, pin_memory=True)
 
-    return dataset, num_classes, extfeat_loader, test_loader, l_data, u_data
-
+    return dataset, num_classes, train_loader, val_loader, test_loader
 
 
 def main(args):
-    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_devices
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
     cudnn.benchmark = True
@@ -171,41 +173,10 @@ def main(args):
     if args.height is None or args.width is None:
         args.height, args.width = (144, 56) if args.arch == 'inception' else \
                                   (256, 128)
-
-    dataset, num_classes, _, test_loader, l_data, u_data = \
-        get_data(args.dataset, args.data_dir, args.height,
-                args.width, args.batch_size, args.workers, args.num_select)
-    if not args.evaluate:
-        if args.dataset == 'dukemtmc':
-            model = models.create(args.arch, num_classes=0)
-        elif args.dataset == 'market1501':
-            model = models.create(args.arch, num_classes=0)
-        elif args.dataset == 'msmt17':
-            model = models.create(args.arch, num_classes=0)
-        else:
-            raise RuntimeError("Please specify the number of classes(ids) of the network")
-        # Load from checkpoint
-        start_epoch = best_top1 = 0
-        if args.resume:
-            print('Resuming checkpoints from finetuned model on another dataset...\n')
-            checkpoint = load_checkpoint(args.resume)
-            model.load_state_dict(checkpoint, strict=False)
-        else:
-            raise RuntimeWarning('Not using a pre-trained model')
-        model = nn.DataParallel(model).cuda()
-
-        eug = EUG(model_name=args.arch, batch_size=args.batch_size, mode=args.mode, num_classes=0, 
-                data_dir=args.data_dir, l_data=l_data, u_data=u_data, print_freq=args.print_freq, save_path=args.logs_dir, pretrained_model=model, rerank=True)
-        # Distance metric
-        metric = DistanceMetric(algorithm=args.dist_metric)
-
-        nums_to_select = len(u_data)
-        pred_y, pred_score = eug.estimate_label()
-        selected_idx = eug.select_top_data(pred_score, nums_to_select)
-        new_train_data = eug.generate_new_train_data(selected_idx, pred_y)
-        eug_dataloader = eug.get_dataloader(new_train_data, training=True)
-        del model
-
+    dataset, num_classes, train_loader, val_loader, test_loader = \
+        get_data(args.dataset, args.split, args.data_dir, args.height,
+                 args.width, args.batch_size, args.num_instances, args.workers,
+                 args.combine_trainval)
     # Create model
     # Hacking here to let the classifier be the last feature embedding layer
     # Net structure: avgpool -> FC(1024) -> FC(args.features)
@@ -213,8 +184,8 @@ def main(args):
 
     # Load from checkpoint
     start_epoch = best_top1 = 0
-    if args.resume_target:
-        checkpoint = torch.load(args.resume_target)
+    if args.resume:
+        checkpoint = load_checkpoint(args.resume)
         model.load_state_dict(checkpoint['state_dict'])
         best_top1 = checkpoint['best_top1']
         print("=> Start epoch {}  best top1 {:.1%}"
@@ -227,8 +198,9 @@ def main(args):
     # Evaluator
     evaluator = Evaluator(model, args.print_freq)
     if args.evaluate:
+        metric.train(model, train_loader)
         print("Test:")
-        evaluator.evaluate(test_loader, dataset.query, dataset.gallery)
+        evaluator.evaluate(test_loader, dataset.query, dataset.gallery, metric)
         return
 
     # Criterion
@@ -242,7 +214,7 @@ def main(args):
                   id(p) not in base_param_ids]
     param_groups = [
         {'params': model.module.base.parameters(), 'lr_mult': 1.0},
-        {'params': new_params, 'lr_mult': 1.0}]
+        {'params': new_params, 'lr_mult': 3.0}]
     # Optimizer
     optimizer = torch.optim.Adam(param_groups, lr=args.lr, 
                                  weight_decay=args.weight_decay)
@@ -251,36 +223,37 @@ def main(args):
     trainer = Trainer(model, criterion, args.print_freq)
     # Schedule learning rate
     def adjust_lr(epoch):
-        lr = args.lr if epoch <= 60 else \
-            args.lr * (0.1 ** (epoch // 60))
-            # args.lr * (0.001 ** ((epoch - 100) / 50.0))
+        lr = args.lr if epoch <= 100 else \
+            args.lr * (0.001 ** ((epoch - 100) / 50.0))
         for g in optimizer.param_groups:
             g['lr'] = lr * g.get('lr_mult', 1)
 
     # Start training
     for epoch in range(start_epoch, args.epochs):
         adjust_lr(epoch)
-        trainer.train(epoch, eug_dataloader, optimizer)
+        trainer.train(epoch, train_loader, optimizer)
         if epoch < args.start_save:
             continue
-        if (epoch % 5 == 0 and epoch > 0) or epoch == args.epochs:
-            top1 = evaluator.evaluate(test_loader, dataset.query, dataset.gallery)
-            is_best = top1 > best_top1
-            best_top1 = max(top1, best_top1)
-            save_checkpoint({
-                'state_dict': model.module.state_dict(),
-                'epoch': epoch + 1,
-                'best_top1': best_top1,
-            }, is_best, fpath=osp.join(args.logs_dir, 'checkpoint.pth.tar'))
+        rank_score = evaluator.evaluate(val_loader, dataset.val, dataset.val)
+        top1 = rank_score.allshots[0]
 
-            print('\n * Finished epoch {:3d}  top1: {:5.1%}  best: {:5.1%}{}\n'.
-                format(epoch, top1, best_top1, ' *' if is_best else ''))
+        is_best = top1 > best_top1
+        best_top1 = max(top1, best_top1)
+        save_checkpoint({
+            'state_dict': model.module.state_dict(),
+            'epoch': epoch + 1,
+            'best_top1': best_top1,
+        }, is_best, fpath=osp.join(args.logs_dir, 'checkpoint.pth.tar'))
+
+        print('\n * Finished epoch {:3d}  top1: {:5.1%}  best: {:5.1%}{}\n'.
+              format(epoch, top1, best_top1, ' *' if is_best else ''))
 
     # Final test
     print('Test with best model:')
-    checkpoint = torch.load(osp.join(args.logs_dir, 'model_best.pth.tar'))
+    checkpoint = load_checkpoint(osp.join(args.logs_dir, 'model_best.pth.tar'))
     model.module.load_state_dict(checkpoint['state_dict'])
-    evaluator.evaluate(test_loader, dataset.query, dataset.gallery)
+    metric.train(model, train_loader)
+    evaluator.evaluate(test_loader, dataset.query, dataset.gallery, metric)
 
 
 if __name__ == '__main__':
@@ -291,10 +264,10 @@ if __name__ == '__main__':
     parser.add_argument('-b', '--batch_size', type=int, default=128)
     parser.add_argument('-j', '--workers', type=int, default=4)
     parser.add_argument('--split', type=int, default=0)
-    parser.add_argument('--height', type=int,default=256,
+    parser.add_argument('--height', type=int,
                         help="input height, default: 256 for resnet*, "
                              "144 for inception")
-    parser.add_argument('--width', type=int, default=128,
+    parser.add_argument('--width', type=int,
                         help="input width, default: 128 for resnet*, "
                              "56 for inception")
     parser.add_argument('--combine-trainval', action='store_true',
@@ -310,8 +283,6 @@ if __name__ == '__main__':
                         choices=models.names())
     parser.add_argument('--features', type=int, default=128)
     parser.add_argument('--dropout', type=float, default=0)
-    parser.add_argument('--mode', type=str,  default="Dissimilarity",
-                        choices=["Classification", "Dissimilarity"])
     # loss
     parser.add_argument('--margin', type=float, default=0.5,
                         help="margin of the triplet loss, default: 0.5")
@@ -321,24 +292,20 @@ if __name__ == '__main__':
     parser.add_argument('--weight-decay', type=float, default=5e-4)
     # training configs
     parser.add_argument('--resume', type=str, default='', metavar='PATH')
-    parser.add_argument('--resume-target', type=str, default='', metavar='PATH')
     parser.add_argument('--evaluate', action='store_true',
                         help="evaluation only")
     parser.add_argument('--epochs', type=int, default=150)
-    parser.add_argument('--start-save', type=int, default=0,
+    parser.add_argument('--start_save', type=int, default=0,
                         help="start saving checkpoints after specific epoch")
     parser.add_argument('--seed', type=int, default=1)
-    parser.add_argument('--print_freq', type=int, default=20)
-    parser.add_argument('--num-select', type=int, default=1)
+    parser.add_argument('--print_freq', type=int, default=10)
     # metric learning
     parser.add_argument('--dist_metric', type=str, default='euclidean',
                         choices=['euclidean', 'kissme'])
     # misc
     working_dir = osp.dirname(osp.abspath(__file__))
-    parser.add_argument('--data-dir', type=str, metavar='PATH',
+    parser.add_argument('--data_dir', type=str, metavar='PATH',
                         default=osp.join(working_dir, 'data'))
-    parser.add_argument('--logs-dir', type=str, metavar='PATH',
+    parser.add_argument('--logs_dir', type=str, metavar='PATH',
                         default=osp.join(working_dir, 'logs'))
-    parser.add_argument('--gpu-devices', default='0,1', type=str, 
-                        help='gpu device ids for CUDA_VISIBLE_DEVICES')
     main(parser.parse_args())
